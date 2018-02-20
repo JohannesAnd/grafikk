120c060e5634b4996d48a9ccc09bc814448609fb
Johannes Andersen <johanes1@online.no>
2018-02-20 14:02:22 +0100

    Working

--- ex1/gloom/src/program.cpp
+++ ex1/gloom/src/program.cpp
@@ -1,8 +1,55 @@
 // Local headers
 #include "program.hpp"
 #include "gloom/gloom.hpp"
-#include "gloom/shader.hpp"
 #include "textures/utilities.hpp"
+#include "gloom/shader.hpp"
+
+int generateVertexArray(float *vertices, float *textureCoordinates, unsigned int *indices, unsigned int vertexCount, unsigned int triangleCount)
+{
+    unsigned int vertexArrayID;
+    glGenVertexArrays(1, &vertexArrayID);
+    glBindVertexArray(vertexArrayID);
+
+    unsigned int vertexBufferID;
+    glGenBuffers(1, &vertexBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertexCount * 3, vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, 0);
+    glEnableVertexAttribArray(0);
+
+    unsigned int textureBufferID;
+    glGenBuffers(1, &textureBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, textureBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertexCount * 2, textureCoordinates, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2, 0);
+    glEnableVertexAttribArray(1);
+
+    unsigned int indexBufferID;
+    glGenBuffers(1, &indexBufferID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * triangleCount * 3, indices, GL_STATIC_DRAW);
+
+    return vertexArrayID;
+}
+
+int loadGLTexture(PNGImage image)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &image.pixels[0]);
+
+    printGLError();
+
+    return textureID;
+}
 
 void runProgram(GLFWwindow *window)
 {
@@ -16,70 +63,38 @@ void runProgram(GLFWwindow *window)
     // Set default colour after clearing the colour buffer
     glClearColor(0.3f, 0.5f, 0.8f, 1.0f);
 
-    // Set up your scene here (create Vertex Array Objects, etc.)
-
-    unsigned int vao = 0;
-
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    unsigned int buffer = 0;
-
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    PNGImage image = loadPNGFile("../gloom/src/textures/diamond.png");
+    std::cout << "Loaded image with size " << image.width << ", " << image.height << std::endl;
+    int textureID = loadGLTexture(image);
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.6f, 0.0f, 0.0f,
-        0.50f, -0.5f, 0.6f, 1.0f, 1.0f,
-        -0.5f, 0.50f, 0.6f, 1.0f, 0.0f};
+        -0.9f, -0.9f, 0,
+        0.9f, -0.9f, 0,
+        0.9f, 0.9f, 0,
+        -0.9f, 0.9f, 0};
+    float textureCoordinates[] = {
+        0, 0,
+        1, 0,
+        1, 1,
+        0, 1};
+    unsigned int indices[] = {
+        0, 1, 2, 0, 2, 3};
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void *)12);
+    unsigned int vertexCount = 4;
+    unsigned int triangleCount = 2;
 
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
+    unsigned int vaoID = generateVertexArray(vertices, textureCoordinates, indices, vertexCount, triangleCount);
 
-    // Index buffer
-
-    unsigned int indexBuffer = 1;
-
-    glGenBuffers(1, &indexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-    unsigned int indexBufferData[] = {0, 1, 2};
-
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexBufferData), &indexBufferData, GL_STATIC_DRAW);
-
-    // Texture
-
-    unsigned int texture = 0;
-
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    PNGImage image = loadPNGFile("../gloom/src/textures/diamond.png");
-
-    std::cout << image.pixels[0] << std::endl;
-
-    glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &image.pixels[0]);
-
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glBindTextureUnit(0, texture);
-
-    // Shaders
+    glBindTextureUnit(0, textureID);
 
     Gloom::Shader shader;
-    shader.makeBasicShader("../gloom/shaders/simple.vert",
-                           "../gloom/shaders/simple.frag");
+    shader.attach("../gloom/shaders/simple.vert");
+    shader.attach("../gloom/shaders/simple.frag");
+    shader.link();
+
+    // Set up your scene here (create Vertex Array Objects, etc.)
 
     // Rendering Loop
-
     while (!glfwWindowShouldClose(window))
     {
         // Clear colour and depth buffers
@@ -87,10 +102,8 @@ void runProgram(GLFWwindow *window)
 
         // Draw your scene here
         shader.activate();
-
-        glBindVertexArray(vao);
-
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(vaoID);
+        glDrawElements(GL_TRIANGLES, 3 * triangleCount, GL_UNSIGNED_INT, 0);
 
         // Handle other events
         glfwPollEvents();
